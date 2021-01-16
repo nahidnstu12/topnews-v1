8e85003988a22a05059b445e6b5ab34957f73bbe
= <=>
2021-01-16 19:54:26 +0600

    git fetch test

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {createStore} from 'redux'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+
+
+export const configureStore = () =>{
+    const store = createStore({
+        // newsResponse : newsResponseReducer
+    })
+}
+
